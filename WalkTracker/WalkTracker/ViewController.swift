ea0f7e271c38f8935db525f89703a2cde637f199
Maria <kobinary@gmail.com>
2019-02-27 17:45:18 +0100

    Created Xcode project

---
+++ WalkTracker/WalkTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WalkTracker
+//
+//  Created by Maria Ortega on 27/02/2019.
+//  Copyright © 2019 Maria Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
